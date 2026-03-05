94aabb5339e30108e562900def9ad182d93e0b7e
Полина Чубыкина <polinakill04@gmail.com>
2026-03-05 19:21:53 +0400

    генерация всех контрактов

---
+++ Department/DepartmentContracts/Attributes/ColumnAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepartmentContracts.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColumnAttribute : Attribute
+    {
+        public ColumnAttribute(string title = "", bool visible = true, int width = 0, GridViewAutoSize gridViewAutoSize = GridViewAutoSize.None, bool isUseAutoSize = false)
+        {
+            Title = title;
+            Visible = visible;
+            Width = width;
+            GridViewAutoSize = gridViewAutoSize;
+            IsUseAutoSize = isUseAutoSize;
+        }
+        public string Title { get; private set; }
+        public bool Visible { get; private set; }
+        public int Width { get; private set; }
+        public GridViewAutoSize GridViewAutoSize { get; private set; }
+        public bool IsUseAutoSize { get; private set; }
+    }
+}
---
+++ Department/DepartmentContracts/Attributes/GridViewAutoSize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepartmentContracts.Attributes
+{
+    public enum GridViewAutoSize
+    {
+        NotSet = 0,
+
+        None = 1,
+
+        ColumnHeader = 2,
+
+        AllCellsExceptHeader = 4,
+
+        AllCells = 6,
+
+        DisplayedCellsExceptHeader = 8,
+
+        DisplayedCells = 10,
+
+        Fill = 16
+    }
+}
